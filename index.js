b5c827a936e59a0b1437d5f3fd1077d5193b6131
ignpoppyseed <87223326+ignpoppyseed@users.noreply.github.com>
2022-08-17 21:01:45 -0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 var express = require("express");
 const fs = require('fs');
 var path = require('path');
-var app = express();app.listen(3000, () => {console.log("Server running on port 3000");});
-app.get("/random", (req, res, next) => {
-
-    const length = fs.readdirSync('./images').length
-    min = Math.ceil(length);
-    max = Math.floor(1);
-    var num = Math.floor(Math.random() * (max - min + 1) + min);
-    var respn1 = 'https://possums.cloverbrand.xyz/images/'+num+'.jpg'
-    res.json({"url": respn1});
-
+var favicon = require('serve-favicon');
+var app = express();app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
 
 app.use('/images', express.static('./images'));
@@ -19,5 +12,15 @@ app.get("/", (req, res, next) => {
 
     res.writeHead(200, {'content-type': 'text/html'});
     fs.createReadStream('index.html').pipe(res)
+
+});
+app.use("/random", (req, res, next) => {
+
+    const length = fs.readdirSync('./images').length
+    min = Math.ceil(length);
+    max = Math.floor(1);
+    var num = Math.floor(Math.random() * (max - min + 1) + min);
+    var respn1 = 'https://possums.cloverbrand.xyz/images/'+num+'.jpg'
+    res.json({"url": respn1});
 
 });
