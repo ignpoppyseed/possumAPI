d01557683990a6a848c43eebe4e95179f6de5a74
ignpoppyseed <87223326+ignpoppyseed@users.noreply.github.com>
2022-08-17 20:24:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+const fs = require('fs');
+var path = require('path');
+var app = express();app.listen(3000, () => {console.log("Server running on port 3000");});
+app.get("/random", (req, res, next) => {
+
+    const length = fs.readdirSync('./images').length
+    min = Math.ceil(length);
+    max = Math.floor(1);
+    var num = Math.floor(Math.random() * (max - min + 1) + min);
+    var respn1 = 'https://possums.cloverbrand.xyz/images/'+num+'.jpg'
+    res.json({"url": respn1});
+
+});
+
+app.use('/images', express.static('./images'));
+
+app.get("/", (req, res, next) => {
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    fs.createReadStream('index.html').pipe(res)
+
+});
